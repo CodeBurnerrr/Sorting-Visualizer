db8ceabee1eaeb24d68c2f59b3d7ea58668ea7ed
Dhruv Mehta <139496882+CodeBurnerrr@users.noreply.github.com>
2025-01-09 19:11:53 +0530

    Added github and linkedin Links

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,10 @@
 import '../Styles/home.css';
 import icon from '../assets/images/right.svg';
 import {useNavigate} from 'react-router-dom';
-import { useState } from 'react';
+import {useState} from 'react';
 import Particle from "../components/particles.tsx";
+import githubIcon from '../assets/images/icons8-github-48.svg';
+import linkedinIcon from '../assets/images/icons8-linkedin.svg';
 
 const Home = () => {
     const [isSliding, setIsSliding] = useState(false);
@@ -16,13 +18,39 @@ const Home = () => {
     }
 
     return (
-        <div className={`heading ${isSliding ? 'slide-out' : ''}`}>
-            <center>Sorting <br/> Visualizer</center>
-            <button className="next-button" onClick={handleRedirect}>
-                <img src={icon} alt="Right Icon" style={{width:'20px'}} />
-            </button>
-            <Particle />
-        </div>
+        <>
+            <div className={`heading ${isSliding ? 'slide-out' : ''}`}>
+                <center>Algorithm <br/> Visualizer</center>
+                <button className="next-button" onClick={handleRedirect}>
+                    <img src={icon} alt="Right Icon" style={{width: '20px'}}/>
+                </button>
+
+                <Particle/>
+            </div>
+            <div className="logo-div">
+                <div className="logo logo-1">
+                    <a
+                        href="https://github.com/CodeBurnerrr/Algorithm-Visualizer"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={githubIcon} alt="GitHub Icon"/>
+                    </a>
+                </div>
+
+                <div className="logo">
+                    <a
+                        href="https://www.linkedin.com/in/dhruvmehta85/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <img src={linkedinIcon} alt="LinkedIn Icon"/>
+                    </a>
+                </div>
+            </div>
+
+
+        </>
     );
 }
 
