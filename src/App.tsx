17240096d978eec1a3d602f14e3655b0348d6451
Dhruv Mehta <139496882+CodeBurnerrr@users.noreply.github.com>
2025-01-20 16:42:59 +0530

    Added Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import Home from './pages/home.tsx'
 import SortingPage from "./pages/sortingPage.tsx";
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
 
 
     return (
         <>
+            <Analytics />
             <BrowserRouter>
                 <Routes>
                     <Route index element={<Home/>}/>
